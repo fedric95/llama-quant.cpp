ee06da18490a3f2ba9d52b691c93277f8763a545
fedric95 <ricciuti.federico@gmail.com>
2024-08-16 14:26:00 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+if [[ ! -v LLAMA_CPP_VERSION ]]; then
+    echo "LLAMA_CPP_VERSION is missing"
+    exit 1
+else
+    echo "LLAMA_CPP_VERSION: $LLAMA_CPP_VERSION"
+fi
+
+if [[ ! -v LLAMA_CPP_CUDA ]]; then
+    echo "LLAMA_CPP_CUDA is missing"
+    exit 1
+else
+    echo "LLAMA_CPP_CUDA: $LLAMA_CPP_CUDA"
+fi
+
+
+
+git clone https://github.com/ggerganov/llama.cpp
+cd llama.cpp
+git reset --hard $LLAMA_CPP_VERSION
+make GGML_CUDA=$LLAMA_CPP_CUDA
+cd ..
+git lfs install
+pip install -r requirements.txt
+./llama.cpp/scripts/get-wikitext-2.sh
