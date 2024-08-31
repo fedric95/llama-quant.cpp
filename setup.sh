86f5749cb3e65fdf5ebdc72f4e1098e878a83b35
fedric95 <ricciuti.federico@gmail.com>
2024-08-31 15:30:19 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 set -e
 
-if [[ ! -v LLAMA_CPP_VERSION ]]; then
-    echo "LLAMA_CPP_VERSION is missing"
-    exit 1
-else
-    echo "LLAMA_CPP_VERSION: $LLAMA_CPP_VERSION"
-fi
-
-if [[ ! -v LLAMA_CPP_CUDA ]]; then
-    echo "LLAMA_CPP_CUDA is missing"
-    exit 1
-else
-    echo "LLAMA_CPP_CUDA: $LLAMA_CPP_CUDA"
-fi
-
-
-
-git clone https://github.com/ggerganov/llama.cpp
-cd llama.cpp
-git reset --hard $LLAMA_CPP_VERSION
-make GGML_CUDA=$LLAMA_CPP_CUDA
-cd ..
+g++ -o convert convert.cpp
+g++ -o encode encode.cpp
